c1b9be4cb1ac7b3dc45ecaaeccefc318da93e820
stephane0713 <stephaned2205@gmail.com>
2021-07-06 16:15:38 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// needed for dev-serv to hot reload on public/index.html change
+import "../public/index.html";
+
+import "./scss/style.scss";
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "js/bundle.js",
+    },
+    mode: mode,
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/style.css",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: { importLoaders: 1 },
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [["autoprefixer", {}]],
+                            },
+                        },
+                    },
+                    "sass-loader",
+                ],
+            },
+            // needed for dev-serv to hot reload on public/index.html change
+            {
+                test: /\.html$/,
+                loader: "raw-loader",
+            },
+        ],
+    },
+};
